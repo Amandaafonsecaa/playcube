03275a9ed1bfb35a4c80008825d0fa8d2d80762b
Amandaafonsecaa <amandafonsecarod09@gmail.com>
2025-09-06 20:25:32 -0300

    refactor: adc axios no index

--- src/api/config/index.ts
+++ src/api/config/index.ts
@@ -1,26 +1,9 @@
-import { q } from '../utils/query'; // Ajuste o caminho
+import axios from "axios";
 
-export const API_CONFIG = {
-  baseURL: import.meta.env.VITE_API_BASE_URL as string,
-  bearer: import.meta.env.VITE_API_BEARER as string,
-};
-
-
-export async function httpGet<T>(path: string, params?: Record<string, any>): Promise<T> {
-    const fullPath = `${path}${q(params)}`
-    const url = new URL(fullPath, API_CONFIG.baseURL);
-
-
-    const res = await fetch(url.toString(), { //explica esse bloco
-        headers: {
-            Accept: "application/json",
-            Authorization: `Bearer ${API_CONFIG.bearer}`
-        },
-    });
-    if(!res.ok){
-        const body = await res.text().catch(()=>"");
-        throw new Error(`TMDB ${res.status}: ${body || res.statusText}`);
-    }
-    return res.json() as Promise<T>;
-
-}
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_TMDB_BASE_URL,
+  headers: {
+    Accept: "application/json",
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_BEARER}`,
+  },
+});
