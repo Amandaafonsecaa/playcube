78d200edb77b2e9e0133f30f6243d58738c89fac
Amandaafonsecaa <amandafonsecarod09@gmail.com>
2025-09-06 20:45:21 -0300

    feat: service das series

---
+++ src/api/services/seriesService.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { api } from "../config";
+
+export function getPopularSeries(page = 1){
+    return api.get("/tv/popular", {
+        params: {
+            language: "pt-BR", region: "BR", page
+        }
+    }).then(r => r.data);
+}
+
+export function getSeriesDetails(id: number){
+    return api.get(`/tv/${id}`, {
+        params: {
+            language: "pt-BR",
+            append_to_response: "credits,images,videos,recommendations,similar",
+        }
+    }).then(r => r.data);
+}
+
+export function getSeriesCredit(id: number){
+    return api.get(`/tv/${id}/credits`, {
+        params: {
+            language: "pt-BR",
+        }
+    }).then(r => r.data);
+}
+
+export function getSeriesVideos(id: number){
+    return api.get(`/tv/${id}/videos`, {
+        params: {
+            language: "pt-BR",
+        }
+    }).then(r => r.data);
+}
