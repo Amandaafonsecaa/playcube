10dfcad1c8bf212e123df1019523c9c0bfeae070
Amandaafonsecaa <amandafonsecarod09@gmail.com>
2025-09-07 09:29:45 -0300

    feat: adc rotas e a lib de img

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 ﻿import Navbar from "./components/navbar"
 import "./App.css"
+import Home from "./pages/home"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
